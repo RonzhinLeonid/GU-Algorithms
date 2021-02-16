962040e643a23a95097eb2d3b7e1541346f83c50
Leonid <jiehuh87@yandex.ru>
2021-02-17 00:29:48 +0300

    Les1 Ex1, Ex3 without test

---
+++ Algorithms/Les1Ex1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Les1Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 30; i++)
+            {
+                if (CheckPrimeNumber(i))
+                {
+                    Console.Write($"{i}, ");
+                }
+            }
+            Console.ReadKey();
+        }
+        /// <summary>
+        /// Проверяет простое число или нет
+        /// </summary>
+        /// <param name="n">Число</param>
+        /// <returns></returns>
+        static bool CheckPrimeNumber(int n)
+        {
+            if(n <= 1) return false;
+            int d = 0;
+            int i = 2;
+            while (i < n)
+            {
+                if (n % i == 0) d++;
+                i++;
+            }
+            if (d == 0) return true;
+            else return false;
+        }
+    }
+}
---
+++ Algorithms/Les1Ex3/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Les1Ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 30; i++)
+            {
+                    Console.WriteLine($"{i} - {GetNumberFibRec(i)}, {GetNumberFib(i)}");
+            }
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Получение числа Фибоначчи по его номеру в ряду рекурсией.
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        static int GetNumberFibRec(int n)
+        {
+            if (n == 0) return 0;
+            if (n == 1) return 1;
+            return GetNumberFibRec(n - 2) + GetNumberFibRec(n - 1);
+        }
+
+        /// <summary>
+        /// Получение числа Фибоначчи по его номеру в ряду циклом.
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        static int GetNumberFib(int n)
+        {
+            int fib1 = 0;
+            int fib2 = 1;
+
+            if (n == 0) return fib1;
+            if (n == 1) return fib2;
+            int fibSum = 0;
+            for (int i = 2; i <= n; i++)
+            {
+                fibSum = fib1 + fib2;
+                fib1 = fib2;
+                fib2 = fibSum;
+            }
+            return fibSum;
+        }
+    }
+}
