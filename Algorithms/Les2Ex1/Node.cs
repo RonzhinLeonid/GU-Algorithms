4915b69e463c695e1741e7ba58f33b527e6c1541
Leonid <jiehuh87@yandex.ru>
2021-02-19 23:39:45 +0300

    Les2

---
+++ Algorithms/Les2Ex1/Node.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Les2Ex1
+{
+    public class Node : ILinkedList
+    {
+        public int Value { get; set; }
+        public Node NextNode { get; set; }
+        public Node PrevNode { get; set; }
+        /// <summary>
+        /// добавляет новый элемент списка
+        /// </summary>
+        /// <param name="value"></param>
+        public void AddNode(int value)
+        {
+            throw new NotImplementedException();
+        }
+        /// <summary>
+        /// добавляет новый элемент списка после определённого элемента
+        /// </summary>
+        /// <param name="node"></param>
+        /// <param name="value"></param>
+        public void AddNodeAfter(Node node, int value)
+        {
+            throw new NotImplementedException();
+        }
+        /// <summary>
+        /// ищет элемент по его значению
+        /// </summary>
+        /// <param name="searchValue"></param>
+        /// <returns></returns>
+        public Node FindNode(int searchValue)
+        {
+            throw new NotImplementedException();
+        }
+        /// <summary>
+        /// возвращает количество элементов в списке
+        /// </summary>
+        /// <returns></returns>
+        public int GetCount()
+        {
+            throw new NotImplementedException();
+        }
+        /// <summary>
+        /// удаляет элемент по порядковому номеру
+        /// </summary>
+        /// <param name="index"></param>
+        public void RemoveNode(int index)
+        {
+            throw new NotImplementedException();
+        }
+        /// <summary>
+        /// удаляет указанный элемент
+        /// </summary>
+        /// <param name="node"></param>
+        public void RemoveNode(Node node)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
