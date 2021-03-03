53a086147224ef1bce94348cacf565866b3904a0
Leonid <jiehuh87@yandex.ru>
2021-03-03 21:49:03 +0300

    upd

--- Algorithms/Les3Ex1/Program.cs
+++ Algorithms/Les3Ex1/Program.cs
@@ -1,32 +1,8 @@
 ﻿using BenchmarkDotNet.Running;
 using System;
-using System.Runtime.InteropServices;
 
 namespace Les3Ex1
 {
-
-    public class PointClass
-    {
-        public int X;
-        public int Y;
-    }
-
-    public struct PointStruct
-    {
-        public int X;
-        public int Y;
-    }
-
-    [StructLayout(LayoutKind.Explicit, Pack = 1)]
-    public struct FloatIntUnion
-    {
-        [FieldOffset(0)]
-        public int i;
-
-        [FieldOffset(0)]
-        public float f;
-    }
-
     class Program
     {
         static void Main(string[] args)
