36773d5b92b410288b9ed487b665d73addb141cb
Leonid <jiehuh87@yandex.ru>
2021-03-11 19:42:03 +0300

    upd

---
+++ Algorithms/Les4Ex1/Program.cs
@@ -0,0 +1,15 @@
+﻿using BenchmarkDotNet.Running;
+using System;
+
+namespace Les4Ex1
+{
+    //Ронжин Л.
+    //1. Протестируйте поиск строки в HashSet и в массиве
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+        }
+    }
+}
